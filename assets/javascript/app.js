6a3a323fdf4e4fda09d4db8590cc475acd160ce0
Matthew Cibulka <mcibulkadev@gmail.com>
2018-05-06 14:07:21 -0400

    Added HTML game structure and timer object that counts down from 10 upon clicking start.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,52 @@
-function playGame ( jQuery) {
-    console.log( "ready!" );
-}
+window.onload = function() {
+    $( "#start" ).click( timer.start );
+};
 
 
-$( document ).ready( playGame() );
+// Gobal Variables
+var intervalId;
+var running = false;
+
+
+// The timer object keeps track of remaining time in each round
+var timer = {
+    remaining: 10,
+
+
+    count: function() {
+        timer.remaining--;
+
+        var $timeRem = $( "#time-remaining " );
+        $timeRem.text( "Time remaining: " + timer.remaining );
+
+        if( timer.remaining === 0 ) {
+            timer.timesUp();
+        }
+    },
+
+
+    start: function() {
+        if (!running) {
+            intervalId = setInterval(timer.count, 1000);
+            running = true;
+            $( "#start" ).hide();
+        }
+    },
+
+
+    stop: function() {
+        clearInterval(intervalId);
+        running = false;
+    },
+
+
+    timesUp: function() {
+        clearInterval(intervalId);
+        running = false;
+        
+        var $timeUp = $( "<h4>" );
+        $timeUp.text( "Time's up!" );
+        $( "#time" ).append( $timeUp );
+    },
+};
+
